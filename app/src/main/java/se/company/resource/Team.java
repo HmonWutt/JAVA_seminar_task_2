9e3b32ea5e5c66d6f4159bce5c668b23461bd19e
hmon <wtthumon@gmail.com>
2025-01-10 14:02:34 +0100

    added methods and attributes

--- app/src/main/java/se/company/resource/Team.java
+++ app/src/main/java/se/company/resource/Team.java
@@ -1,1 +1,36 @@
 package se.company.resource;
+public class Team {
+    private ArrayList<Employee> members = new ArrayList<>();
+
+    public void add(Employee empolyee) {
+        members.add(employee);
+    }
+
+    public String toString(){
+        String teamMembersAndDepartments = "# TEAM:\n";
+        for (Employee member : members) {
+            String task = member.work();
+            if (!task.isEmpty()) {
+                String employee = String.format("Emp: %s (%s)",member.name,member.work );
+                teamMembersAndDepartments+=employee("\n");
+            }
+        }
+        return teamMembersAndDepartments;
+    }
+
+    public String work() {
+        String workBeingDoneByTeam = "# TEAM is working:\n";
+        for (Employee member : members) {
+            String task = member.work();
+            if (!task.isEmpty()) {
+                String workBeingDoneByEmployee = String.format("Emp: %s is working on a report in %s.",member.name,member.work );
+                workBeingDone+=workBeingDoneByEmployee("\n");
+            }
+        }
+        return workBeingDoneByTeam;
+    }
+
+    public ArrayList<Employee> getMembers() {
+        return members;
+    }
+}
